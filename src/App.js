5c62f7da5b9576db78aef1dab65b58e1dd2156a7
Mitchell Flood <mitchflood1@gmail.com>
2023-07-10 13:02:11 -0400

    adding structure to form for frontend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import './Style.css';
 
 function App() {
   return (
   <>
-  <label htmlFor="name">Name</label>
-  <input type="text" name="name" id="name" />
-  <label htmlFor="mobile">Mobile</label>
-  <input type="text" name="mobile" id="mobile" />
-  <label htmlFor="mobile">Email</label>
-  <input type="text" name="email" id="email" />
-  <label htmlFor="mobile">Email</label>
-  <input type="button" name="send" id="send" value="SEND" />
+  <div className="container">
+    <label htmlFor="name">Name</label>
+    <input type="text" name="name" id="name" />
+    <label htmlFor="mobile">Mobile</label>
+    <input type="text" name="mobile" id="mobile" />
+    <label htmlFor="mobile">Email</label>
+    <input type="text" name="email" id="email" />
+    <label htmlFor="mobile">Email</label>
+    <input type="button" name="send" id="send" value="SEND" />
+  </div>
   </>
   );
 }
