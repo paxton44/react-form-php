77aa400982e4a3dbfe266d1719209c89dff47878
Mitchell Flood <mitchflood1@gmail.com>
2023-07-10 12:53:44 -0400

    messing around with php and react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+  <>
+  <label htmlFor="name">Name</label>
+  <input type="text" name="name" id="name" />
+  <label htmlFor="mobile">Mobile</label>
+  <input type="text" name="mobile" id="mobile" />
+  <label htmlFor="mobile">Email</label>
+  <input type="text" name="email" id="email" />
+  <label htmlFor="mobile">Email</label>
+  <input type="button" name="send" id="send" value="SEND" />
+  </>
+  );
+}
+
+export default App;
