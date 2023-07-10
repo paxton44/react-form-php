80474174de5249ce4fa6083f8825e095906a6e21
Mitchell Flood <mitchflood1@gmail.com>
2023-07-10 13:36:19 -0400

    finished basic frontend with logic now starting php backend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import './Style.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [name, setName] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {
+    if(name.length === 0) {
+      alert("Name has been left blank!");
+    }
+    else if(mobile.length === 0) {
+      alert("Mobile Number has been left blank!");
+    }
+    else if(email.length === 0) {
+      alert("Email Address has been left blank!");
+    }
+    else{
+      const url = "http://localhost/enquiry.php";
+      let fData = new FormData();
+      fData.append('name', name);
+      fData.append('mobile', mobile);
+      fData.append('email', email);
+      
+      axios.post(url, fData)
+      .then(response=> alert(response.data))
+      .catch(error=> alert(error));
+    }
+  }
   return (
   <>
   <div className="container">
     <label htmlFor="name">Name</label>
-    <input type="text" name="name" id="name" />
+    <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
     <label htmlFor="mobile">Mobile</label>
-    <input type="text" name="mobile" id="mobile" />
-    <label htmlFor="mobile">Email</label>
-    <input type="text" name="email" id="email" />
-    <label htmlFor="mobile">Email</label>
-    <input type="button" name="send" id="send" value="SEND" />
+    <input type="text" name="mobile" id="mobile" value={mobile} onChange={(e) => setMobile(e.target.value)} />
+    <label htmlFor="email">Email</label>
+    <input type="text" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    <input type="button" name="send" id="send" value="SEND" onClick={handleSubmit} />
   </div>
   </>
   );
